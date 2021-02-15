a906703039081285efd060761d2734e0eade3ea8
Bas Tichelaar <bas@tichelaar.io>
2021-02-15 03:24:12 +0100

    Fixed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ for resume in $FOLDERS
 do
     config=`basename $resume`
     echo $config
-    hackmyresume build ./resumes/${config}/resume.json to /out/{config}.pdf --theme skyworkz
-    hackmyresume build ./resumes/${config}/resume.json to /out/{config}.pdf --theme skyworkz
+    hackmyresume build ./resumes/${config}/resume.json to /out/${config}.pdf --theme skyworkz
+    hackmyresume build ./resumes/${config}/resume.json to /out/${config}.doc --theme skyworkz
 done
