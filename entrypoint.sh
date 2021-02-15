add252c3e2143fd8536a7f234a2d7338f94d5703
Bas Tichelaar <bas@tichelaar.io>
2021-02-15 22:17:33 +0100

    Use full name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ cd $GITHUB_WORKSPACE
 FOLDERS=./resumes/*
 for resume in $FOLDERS
 do
-    config=`basename $resume`
-    echo $config
-    hackmyresume build $GITHUB_WORKSPACE/resumes/${config}/resume.json to $GITHUB_WORKSPACE/out/${config}.pdf --theme skyworkz
-    hackmyresume build $GITHUB_WORKSPACE/resumes/${config}/resume.json to $GITHUB_WORKSPACE/out/${config}.doc --theme skyworkz
+    config=$(basename "$resume")
+    echo hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}/CV ${config} - Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}/CV ${config} - Skyworkz.pdf" --theme skyworkz
 done
