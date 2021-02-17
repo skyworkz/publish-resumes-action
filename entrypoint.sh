c05f17c90be32c82faf297be59b0ae1982f35e8a
Bas Tichelaar <bas@tichelaar.io>
2021-02-17 11:50:49 +0100

    Removed echo statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ FOLDERS=./resumes/*
 for resume in $FOLDERS
 do
     config=$(basename "$resume")
-    echo hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}/CV ${config} - Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}/CV ${config} - Skyworkz.pdf" --theme skyworkz
+    hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}/CV ${config} - Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}/CV ${config} - Skyworkz.pdf" --theme skyworkz
 done
