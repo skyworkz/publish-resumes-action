322e6a377652d77ed70470b86dcd0fad17856151
Nidhi Sharma <nidhi@skyworkz.nl>
2022-08-17 12:55:40 +0200

    removed changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,4 @@ for resume in $FOLDERS
 do
     config=$(basename "$resume")
     hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.pdf" --theme skyworkz
-    #sanitize doc files
-    sed -i 's#\&amp;#\&#g' "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc"
-    sed -i 's#\&apos;#\'#g' "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc"
 done
