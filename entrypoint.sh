f5a02653e4cadcbc81a72839b4cfc5b3e0309c57
Nidhi Sharma <nidhi@skyworkz.nl>
2022-08-17 14:54:57 +0200

    sanitize doc files

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ FOLDERS=./resumes/*
 for resume in $FOLDERS
 do
     config=$(basename "$resume")
-    hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.pdf" --theme skyworkz --no-escape
+    hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.pdf" --theme skyworkz
+    sed -i "s|\&amp;|\&|g" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc"
+    sed -i "s|\&apos;|\'|g" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc"
 done
