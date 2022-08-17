3dbb732d2199902777320f6e0c427ae45781b32b
Nidhi Sharma <nidhi@skyworkz.nl>
2022-08-17 12:27:13 +0200

    disable escape for theme

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ FOLDERS=./resumes/*
 for resume in $FOLDERS
 do
     config=$(basename "$resume")
-    hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.pdf" --theme skyworkz
+    hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" --no-escape to "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.pdf" --theme skyworkz
 done
