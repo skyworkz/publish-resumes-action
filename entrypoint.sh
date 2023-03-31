93e6f6b38ceaa04acd21d9ff6ad7496da6177df2
Benny Cornelissen <mail@bennycornelissen.nl>
2023-03-31 16:37:23 +0200

    Update entrypoint.sh
    Add step for validation of resumes, and slightly more verbose output so we can tell which one is failing

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,16 @@ FOLDERS=./resumes/*
 for resume in $FOLDERS
 do
     config=$(basename "$resume")
+    
+    echo "Validating resume $config"
+    hackmyresume validate "$GITHUB_WORKSPACE/resumes${config}/resume.json" --assert
+    
+    echo "Building resume $config"
     hackmyresume build "$GITHUB_WORKSPACE/resumes/${config}/resume.json" to "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.pdf" --theme skyworkz
+    
     sed -i "s|\&amp;|\&|g" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc"
     sed -i "s|\&apos;|\'|g" "$GITHUB_WORKSPACE/out/${config}_Skyworkz.doc"
+
 done
+
+echo "Completed all resumes"
